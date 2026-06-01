e0f8ede24cd63bec6d09b9ae90415955aa9d92a9
Simran Singh <ss.simransingh26@hmail.com>
2026-06-01 22:08:48 +0530

    pytorch version and a typo

--- model/predictor.py
+++ model/predictor.py
@@ -1,4 +1,5 @@
 import torch
+torch.set_num_threads(1)
 from torchvision import transforms
 from PIL import Image
 import torch.nn as nn
@@ -16,7 +17,7 @@ LABELS=['Melanocytic nevi',
 
 #loading pre trained model
 def load_model():
-    model=timm.create_model('efficientnet_b0',pretrained=True,num_classes=7)
+    model=timm.create_model('efficientnet_b0',pretrained=True, num_classes=7)
     model.eval()
     return model
 
@@ -24,7 +25,7 @@ def transform_image(image_path):#preprocess uploaded image before prediction
     transform=transforms.Compose([
         transforms.Resize((224,224)),
         transforms.ToTensor(),
-        transforms.Normalize([0.485,0.456,0.406],[0.229,2.224,0.225])
+        transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])
         ])
     image = Image.open(image_path).convert('RGB')
     return transform(image).unsqueeze(0)
